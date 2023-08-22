8d405c5870976e60cfffa203aff246cdb35d6625
ricmonmol <jacksonfkk@gmail.com>
2023-08-22 19:44:24 -0300

    card controller

---
+++ models/card.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    trim: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: Date,
+});
+
+export default mongoose.model("Event", eventSchema);
